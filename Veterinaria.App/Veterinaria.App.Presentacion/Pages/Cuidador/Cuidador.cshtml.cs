15905207f10aa8dac8227bfefd627aaa4279a2a9
JorgeVasquez423 <jorgevasquez423@gmail.com>
2021-09-29 23:06:55 -0500

    Pagina Cuidador

---
+++ Veterinaria.App/Veterinaria.App.Presentacion/Pages/Cuidador/Cuidador.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Veterinaria.App.Presentacion.Pages
+{
+    public class CuidadorModel : PageModel
+    {
+        private readonly ILogger<CuidadorModel> _logger;
+
+        public CuidadorModel(ILogger<CuidadorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
