27e984415e8b3723cf2fc26171e89734f3ee35f2
MiguelPasaje <cartel-dy@hotmail.com>
2021-10-06 20:17:36 -0500

    se empieza a implementar el modelo .cs

--- Veterinaria.App/Veterinaria.App.Presentacion/Pages/Mascota/Mascota.cshtml.cs
+++ Veterinaria.App/Veterinaria.App.Presentacion/Pages/Mascota/Mascota.cshtml.cs
@@ -6,10 +6,19 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
 namespace Veterinaria.App.Presentacion.Pages
 {
     public class MascotaModel : PageModel
     {
+
+        /* lista de mascotas */
+        public List<Mascotax> listaMascotas = new List<Mascotax>();
+
+        /*------  */
         private readonly ILogger<MascotaModel> _logger;
 
         public MascotaModel(ILogger<MascotaModel> logger)
@@ -19,6 +28,24 @@ namespace Veterinaria.App.Presentacion.Pages
 
         public void OnGet()
         {
+            // add mascota a la lista
+            this.listaMascotas.Add( new Mascotax{
+                Nombre = "Rayo",
+                Especie = "Perro",
+                Raza = "pitbull",
+                Edad = "2"
+            } );
         }
+    }
+
+
+    public class Mascotax{
+
+        public String Nombre{get;set;}
+        public String Especie{get;set;}
+        public String Raza{get;set;}
+        public String Edad{get;set;}
+
+
     }
 }
