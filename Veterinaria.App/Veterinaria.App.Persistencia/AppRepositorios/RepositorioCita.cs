715ed6c7fbc86af96744efcd72c1df0b7acfc086
MiguelPasaje <cartel-dy@hotmail.com>
2021-10-07 10:37:18 -0500

    codigo crud RepositorioCita completo

--- Veterinaria.App/Veterinaria.App.Persistencia/AppRepositorios/RepositorioCita.cs
+++ Veterinaria.App/Veterinaria.App.Persistencia/AppRepositorios/RepositorioCita.cs
@@ -16,17 +16,67 @@ namespace Veterinaria.App.Persistencia
 
         /*  */
         public Cita addCita(Cita cita){
-            return null;
+            var citaAdicionado = this.appContext.Citas.Add(cita);
+            this.appContext.SaveChanges();
+            Console.WriteLine("Cita agregado");
+
+            return citaAdicionado.Entity;
              
          }
-        public Cita editCita(Cita cita){
+        public Cita editCita(Cita citaNuevo){
+            var citaEncontrado =
+                this
+                    .appContext
+                    .Citas
+                    .FirstOrDefault(v => v.Id == citaNuevo.Id);
+
+            if (citaEncontrado != null)
+            {                
+                citaEncontrado.Fecha = citaNuevo.Fecha;
+                
+                this.appContext.SaveChanges();
+            }
+
             return null;
         }
-        public void eliminarCita(int cita){
-            return ;
+        public void eliminarCita(int idCita){
+            var citaEncontrado =
+                this
+                    .appContext
+                    .Citas
+                    .FirstOrDefault(v => v.Id == idCita);
+
+            if (citaEncontrado == null)
+            {
+                //return null;
+                //var x=5;
+                Console
+                    .WriteLine("no existe el id de la cita que desea eliminar");
+                return;
+            }
+            else
+            {
+                this.appContext.Citas.Remove(citaEncontrado);
+                this.appContext.SaveChanges();
+                Console.WriteLine("Cita Eliminado");
+            }
         }
-        public Cita GetCita(int cita){
-            return null;
+        public Cita GetCita(int idCita){
+            var citaEncontrado =
+                this
+                    .appContext
+                    .Citas
+                    .FirstOrDefault(v => v.Id == idCita);
+
+            if (citaEncontrado != null)
+            {
+                return citaEncontrado;
+            }
+            else
+            {
+                Console.WriteLine("Cita no encontrada");
+                return null;
+            }
         }
         public IEnumerable<Cita> GetCita(){
             return null;
