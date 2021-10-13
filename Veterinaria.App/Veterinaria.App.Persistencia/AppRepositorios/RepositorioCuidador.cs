c618ec80e1102b831cf28173a966bafd8e0a9472
MiguelPasaje <cartel-dy@hotmail.com>
2021-10-13 17:31:48 -0500

    crud cuidador con bd funcional, falta btn view

--- Veterinaria.App/Veterinaria.App.Persistencia/AppRepositorios/RepositorioCuidador.cs
+++ Veterinaria.App/Veterinaria.App.Persistencia/AppRepositorios/RepositorioCuidador.cs
@@ -94,7 +94,7 @@ namespace Veterinaria.App.Persistencia
 
         public IEnumerable<Cuidador> GetCuidadores()
         {
-            return null;
+            return this.appContext.Cuidadores;
         }
     }
 }
--- Veterinaria.App/Veterinaria.App.Presentacion/Pages/Cuidador/Cuidador.cshtml.cs
+++ Veterinaria.App/Veterinaria.App.Presentacion/Pages/Cuidador/Cuidador.cshtml.cs
@@ -6,10 +6,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+using Veterinaria.App.Dominio;
+using Veterinaria.App.Persistencia;
+
 namespace Veterinaria.App.Presentacion.Pages
 {
     public class CuidadorModel : PageModel
     {
+        /*  */
+        private static IRepositorioCuidador repositorioCuidador = new RepositorioCuidador(new Persistencia.AppContext());
+
+        public IEnumerable<Cuidador> listaCuidadores = new List<Cuidador>();
+        
+        public Cuidador CuidadorDeEdicion;
+        public String modoEdicion = "adicion";
+
+
+        /*  */
+
+
         private readonly ILogger<CuidadorModel> _logger;
 
         public CuidadorModel(ILogger<CuidadorModel> logger)
@@ -17,8 +32,40 @@ namespace Veterinaria.App.Presentacion.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public void OnGet(int idCuidador)
         {
+            if (idCuidador > 0)
+            {
+                this.CuidadorDeEdicion = repositorioCuidador.GetCuidador(idCuidador);     
+                this.modoEdicion = "edicion";
+                                    
+            }else
+            {
+                this.modoEdicion ="adicion";
+            }
+            
+            this.listaCuidadores = repositorioCuidador.GetCuidadores();
+        }
+        public void OnPostAdd(Cuidador CuidadorNuevo){
+            repositorioCuidador.addCuidador (CuidadorNuevo);
+
+            //
+            this.listaCuidadores = repositorioCuidador.GetCuidadores();
+        }
+
+        public void OnPostDel(int IdCuidador){
+            repositorioCuidador.eliminarCuidador (IdCuidador);
+
+            //
+            this.listaCuidadores = repositorioCuidador.GetCuidadores();
+        }
+
+        public void OnPostEdit(Cuidador CuidadorNuevo){
+            repositorioCuidador.editCuidador(CuidadorNuevo);
+            //Console.WriteLine("nombre: " + CuidadorNuevo.Especie);
+
+            //
+            this.listaCuidadores = repositorioCuidador.GetCuidadores();
         }
     }
 }
